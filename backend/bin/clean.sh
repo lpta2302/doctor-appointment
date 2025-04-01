a261b2b7571eff7c08cf873e6c2696c8d4019efb
lpta2302 <lpta2302@gmail.com>
2025-04-01 23:23:06 +0700

    fix java version

--- backend/bin/clean.sh
+++ backend/bin/clean.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo "🚀 Stop Docker Compose and Remove volume..."
-docker compose down -v
+docker compose down
 
 echo "🏗️  Remove all services..."
-SERVICES=($(cat services.txt))
+SERVICES=($(cat services.txt | tr -d '\r'))
 
 for SERVICE in "${SERVICES[@]}"; do
   echo "🔨 Removing $SERVICE..."
-  docker rmi backend-$SERVICE
+  docker rmi backend-$SERVICE -f
 done
 
 echo "✅ All services have been removed successfully!"
